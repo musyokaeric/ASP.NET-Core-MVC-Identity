8eed1e7c15e0c96f070049afe1b772f190beb7ed
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-12 14:16:17 +0300

    Register First User

--- IdentityManager/Controllers/AccountController.cs
+++ IdentityManager/Controllers/AccountController.cs
@@ -1,14 +1,49 @@
-﻿using IdentityManager.Models.ViewModels;
+﻿using IdentityManager.Models;
+using IdentityManager.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityManager.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         public IActionResult Register()
         {
             RegisterViewModel registerViewModel = new RegisterViewModel();
             return View(registerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name,
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View(model);
         }
     }
 }
