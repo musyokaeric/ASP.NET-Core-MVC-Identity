a7b7128e2bc89dcfc293f0397515648fee65e050
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-12 13:17:34 +0300

    Login and Register View Models

---
+++ IdentityManager/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManager.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The password must be {2} characters long", MinimumLength = 6)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
