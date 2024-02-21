4c6256ce0b104ef479c518026c2c8b194852ef38
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-21 20:54:45 +0300

    Role List

---
+++ IdentityManager/Controllers/RoleController.cs
@@ -0,0 +1,22 @@
+﻿using IdentityManager.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public RoleController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = context.Roles.ToList();
+
+            return View(roles);
+        }
+    }
+}
