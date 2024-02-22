86af31897f7a8e6ad4aea46a33fb6d0fdbd746f8
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-22 10:35:35 +0300

    Added Endpoint to Manage Roles

--- IdentityManager/Controllers/UserController.cs
+++ IdentityManager/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityManager.Data;
 using IdentityManager.Models;
+using IdentityManager.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +10,13 @@ namespace IdentityManager.Controllers
     {
         private readonly ApplicationDbContext context;
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
 
-        public UserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        public UserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             this.context = context;
             this.userManager = userManager;
+            this.roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -36,6 +39,29 @@ namespace IdentityManager.Controllers
             }
 
             return View(users);
+        }
+
+        public async Task<IActionResult> ManageRole(string userId)
+        {
+            ApplicationUser user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            List<string> existingUserRoles = await userManager.GetRolesAsync(user) as List<string>;
+            var model = new RolesViewModel { User = user };
+
+            foreach (var role in roleManager.Roles)
+            {
+                RoleSelection roleSelection = new RoleSelection { RoleName = role.Name };
+                if (existingUserRoles.Any(r => r == role.Name))
+                {
+                    roleSelection.IsSelected = true;
+                }
+                model.RolesList.Add(roleSelection);
+            }
+            return View(model);
         }
     }
 }
